8ddbb6f63b0ad3eea3646cd8dd7ee9f4c1fe8221
Sayeda Sultana <sayeda.nsultana@gmail.com>
2024-12-01 01:08:05 +0000

    commit

--- models/example/location_dim.sql
+++ models/example/location_dim.sql
@@ -1,30 +1,53 @@
 -- models/location_dimension.sql
 {{ config(materialized="table") }}
 
-WITH raw_location_data AS (
+{{ config(materialized="table") }}
+
+WITH combined_location_data AS (
+    -- Extract data from FoodEstablishment dataset
     SELECT DISTINCT
         location_type,
-        borough,
-        zipcode
+        borough AS city_borough,
+        community_board,
+        street_name AS street_address,
+        incident_zip AS zipcode
     FROM `cis9440gp.RawDataset.FoodEstablishment`
     WHERE location_type IS NOT NULL
+
+    UNION DISTINCT
+
+    -- Extract data from RestaurantInspection dataset
+    SELECT DISTINCT
+        inspection_type AS location_type,
+        boro AS city_borough,
+        community_board,
+        street AS street_address,
+        zipcode
+    FROM `cis9440gp.RawDataset.RestaurantInspection`
+    WHERE inspection_type IS NOT NULL
 ),
 
 unique_location_ids AS (
+    -- Assign unique IDs to each distinct location
     SELECT 
-        ROW_NUMBER() OVER (ORDER BY location_type, borough, zipcode) AS location_dim_id,
+        ROW_NUMBER() OVER (ORDER BY location_type, city_borough, zipcode) AS location_dim_id,
         location_type,
-        borough,
-        zipcode
-    FROM raw_location_data
+        city_borough,
+        community_board,
+        zipcode,
+        street_address  -- No comma here
+    FROM combined_location_data
 )
 
+-- Final location dimension
 SELECT 
     location_dim_id,
     location_type,
-    borough,
+    city_borough AS borough,
+    community_board,
     zipcode,
-    -- Optional categorization based on location_type
+    street_address,
+    -- Categorize location types into broader categories
     CASE 
         WHEN location_type IN ('Cafeteria - College/University', 'Cafeteria - Private School', 'Cafeteria - Public School') THEN 'Educational'
         WHEN location_type IN ('Food Cart Vendor', 'Mobile Food Vendor', 'Street Vendor', 'Street Fair Vendor') THEN 'Mobile Service'
@@ -33,3 +56,6 @@ SELECT
         ELSE 'Other'
     END AS location_category
 FROM unique_location_ids
+
+
+
